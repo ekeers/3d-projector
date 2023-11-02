fecf50ad0c2c0bea7c6bd1b73b853d7ace5118dd
Ekers <105935934+ekeers@users.noreply.github.com>
2023-11-02 22:50:15 +0100

    rotacion
    finaly, rotation has been achived

--- renderer.py
+++ renderer.py
@@ -1,6 +1,9 @@
 import turtle
-import time
+import math
 import keyboard
+import time
+
+
 triangleFill = False
 turtle.bgcolor('black')
 
@@ -14,10 +17,10 @@ vertexList = [[ 2,-2, 4],           #XYZ
               [ 2, 2, 4],
               [-2,-2, 4],
               [-2, 2, 4],
-              [ 2,-2, 6],
-              [ 2, 2, 6],
-              [-2,-2, 6],
-              [-2, 2, 6]]
+              [ 2,-2, 8],
+              [ 2, 2, 8],
+              [-2,-2, 8],
+              [-2, 2, 8]]
 
 triangleList = [[1,2,3],
                 [2,3,4],
@@ -44,7 +47,7 @@ screenDistance = 0
 focalDistance = 3
 
 nugeAmount = 0.25
-
+turnAmount = math.radians(1)
 
 def drawTriangle(x):
     dw.goto(-screenY[triangleList[x][0] - 1], -screenX[triangleList[x][0]  - 1])
@@ -110,25 +113,67 @@ while True:
         for i in range(vertexNumber):
             vertexList[i][1] = vertexList[i][1] - nugeAmount
             renderDo+=1
-    if keyboard.is_pressed('s'):
+    if keyboard.is_pressed('c'):
         for i in range(vertexNumber):
             vertexList[i][0] = vertexList[i][0] + nugeAmount
             renderDo+=1
-    if keyboard.is_pressed('w'):
+    if keyboard.is_pressed(' '):
         for i in range(vertexNumber):
             vertexList[i][0] = vertexList[i][0] - nugeAmount
             renderDo+=1
-    if keyboard.is_pressed('f'):
+    if keyboard.is_pressed('s'):
         for i in range(vertexNumber):
             vertexList[i][2] = vertexList[i][2] + nugeAmount
             renderDo+=1
-    if keyboard.is_pressed('r'):
+    if keyboard.is_pressed('w'):
         for i in range(vertexNumber):
             vertexList[i][2] = vertexList[i][2] - nugeAmount
-            renderDo+=1    
+            renderDo+=1
+    if keyboard.is_pressed('up'):
+        for i in range(vertexNumber):
+            vertexList[i][0] = vertexList[i][0] * math.cos(turnAmount)  + vertexList[i][1] * 0 + vertexList[i][2] * -math.sin(turnAmount)
+            vertexList[i][1] = vertexList[i][0] * 0                     + vertexList[i][1] * 1 + vertexList[i][2] * 0
+            vertexList[i][2] = vertexList[i][0] * math.sin(turnAmount)  + vertexList[i][1] * 0 + vertexList[i][2] * math.cos(turnAmount)
+            renderDo+=1
+    if keyboard.is_pressed('down'):
+        for i in range(vertexNumber):
+            vertexList[i][0] = vertexList[i][0] * math.cos(-turnAmount)  + vertexList[i][1] * 0 + vertexList[i][2] * -math.sin(-turnAmount)
+            vertexList[i][1] = vertexList[i][0] * 0                     + vertexList[i][1] * 1 + vertexList[i][2] * 0
+            vertexList[i][2] = vertexList[i][0] * math.sin(-turnAmount)  + vertexList[i][1] * 0 + vertexList[i][2] * math.cos(-turnAmount)
+            renderDo+=1
+    if keyboard.is_pressed('right'):
+        for i in range(vertexNumber):
+            vertexList[i][0] = vertexList[i][0] * 1  + vertexList[i][1] * 0                    + vertexList[i][2] * 0
+            vertexList[i][1] = vertexList[i][0] * 0  + vertexList[i][1] * math.cos(turnAmount) + vertexList[i][2] * -math.sin(turnAmount)
+            vertexList[i][2] = vertexList[i][0] * 0  + vertexList[i][1] * math.sin(turnAmount) + vertexList[i][2] * math.cos(turnAmount)
+            renderDo+=1
+    if keyboard.is_pressed('left'):
+        for i in range(vertexNumber):
+            vertexList[i][0] = vertexList[i][0] * 1  + vertexList[i][1] * 0                    + vertexList[i][2] * 0
+            vertexList[i][1] = vertexList[i][0] * 0  + vertexList[i][1] * math.cos(-turnAmount) + vertexList[i][2] * -math.sin(-turnAmount)
+            vertexList[i][2] = vertexList[i][0] * 0  + vertexList[i][1] * math.sin(-turnAmount) + vertexList[i][2] * math.cos(-turnAmount)
+            renderDo+=1
+    if keyboard.is_pressed('e'):
+        for i in range(vertexNumber):
+            vertexList[i][0] = vertexList[i][0] * math.cos(turnAmount)  + vertexList[i][1] * -math.sin(turnAmount) + vertexList[i][2] * 0
+            vertexList[i][1] = vertexList[i][0] * math.sin(turnAmount)  + vertexList[i][1] * math.cos(turnAmount)  + vertexList[i][2] * 0
+            vertexList[i][2] = vertexList[i][0] * 0                     + vertexList[i][1] * 0                     + vertexList[i][2] * 1
+            renderDo+=1
+    if keyboard.is_pressed('q'):
+        for i in range(vertexNumber):
+            vertexList[i][0] = vertexList[i][0] * math.cos(-turnAmount)  + vertexList[i][1] * -math.sin(-turnAmount) + vertexList[i][2] * 0
+            vertexList[i][1] = vertexList[i][0] * math.sin(-turnAmount)  + vertexList[i][1] * math.cos(-turnAmount)  + vertexList[i][2] * 0
+            vertexList[i][2] = vertexList[i][0] * 0                      + vertexList[i][1] * 0                      + vertexList[i][2] * 1
+            renderDo+=1
+
+
+
+
+
     if renderDo > 0:
         render()
         renderDo = 0
+
 
 
 
